c8265b1b21a64f73785b0a91efe8786d3181e215
Abhiram Kosuru <abhiramkosuru967@gmail.com>
2026-01-15 19:31:51 +0530

    Delete CompanyRepository.java

--- CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.placementtracker.repository;
-import com.example.placementtracker.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Optional<Company> findByName(String name);
-}
